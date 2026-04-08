40e74bd1b3d2d07d16e38e7bd178b875dbdffa94
IsmaiKhan0811 <ismailkhan0811@gmail.com>
2026-04-08 17:12:50 +0530

    Left rotate array by one

---
+++ Leftrotatebyone.java
@@ -0,0 +1,19 @@
+public class Leftrotatebyone {
+    public static void rotatebyone( int arr[]){
+        int n = arr.length;
+        int temp[] = new int[n];
+        for(int i =1; i<n;i++){
+            temp[i-1]=arr[i];
+        }
+        temp[n-1]=arr[0];
+        for(int i=0; i<n;i++){
+            System.out.print(temp[i] + " ");
+        }
+    }
+    public static void main(String[] args ){
+       
+        int arr[]={1,2,3,4,5};
+        rotatebyone(arr);
+    }
+    
+}
